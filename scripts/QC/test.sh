716efb9692cba2c25abe48c047d90f9e4c983528
Nadia Atallah <natallah@purdue.edu>
2015-05-11 17:16:39 -0400

    added slides/QC/figures

--- scripts/QC/test.sh
+++ scripts/QC/test.sh
@@ -9,8 +9,13 @@ DS_path=$REPO/datasets/QC_test
 ls $DS_path
 
 REPORT_PATH=f020_res_fastqc
+
+# put if not exist on folder here before mkdir
 mkdir $REPORT_PATH
-fastqc -o $REPORT_PATH $DS_path/f010_raw_mirna.fastq
+
+#fastqc -o $REPORT_PATH $DS_path/f010_raw_mirna.fastq
+
+echo Run this from scripts/QC folder: fastqc -o $REPORT_PATH $DS_path/f010_raw_mirna.fastq
 
 
 
